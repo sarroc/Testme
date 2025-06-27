4eca92fcad6542cfec65cd3cee3570e876f0c1a4
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-27 21:48:22 +0200

    fiix testing

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -19,5 +19,19 @@ it('shows a success message with a valid email', async () => {
 
     const h1 = dom.window.document.querySelector('h1');
 
-    console.log('Contents of h1', h1.innerHTML);
+    assert.strictEqual(h1.innerHTML, 'Looks good!');
+});
+
+it('shows a fail message with an invalid email', async () => {
+    const dom = await render('index.html');
+
+    const input = dom.window.document.querySelector('input');
+    input.value = 'hellodarling';
+    dom.window.document
+        .querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'));
+
+    const h1 = dom.window.document.querySelector('h1');
+
+    assert.strictEqual(h1.innerHTML, 'Invalid email');
 });
